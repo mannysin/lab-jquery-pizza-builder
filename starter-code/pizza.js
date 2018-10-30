17814c37b9724d4e91eabeb0d86f40dff7dd1eec
mannysin <sinclair2610@gmail.com>
2018-10-30 16:06:47 -0400

    working on price iteration

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,1 +1,67 @@
 // Write your Pizza Builder JavaScript in this file.
+window.onload = function(){
+
+
+
+    $('.pep').toggle()
+    $('.mushroom').toggle()
+    $('.green-pepper').toggle()
+    // $('.sauce').toggle()
+    // $('.crust').toggle()
+
+    $('.btn-pepperonni').click(()=>{
+            $('.btn-pepperonni').toggleClass('active');
+            $('.pep').toggle()
+            if ($('.btn-pepperonni').hasClass('active')){
+                $('.btn-pepperonni':visible)
+            }; else {
+                $('.btn-pepperonni:hidden')
+            }
+
+    })
+
+    $('.btn-mushrooms').click(()=>{
+        $('.btn-mushrooms').toggleClass('active');
+        $('.mushroom').toggle()
+    })
+
+    $('.btn-green-peppers').click(()=>{
+        $('.btn-green-peppers').toggleClass('active');
+        $('.green-pepper').toggle()
+    })
+    
+    $('.btn-sauce').click(()=> {
+        $('.btn-sauce').toggleClass('active');
+        $('.sauce').toggleClass('sauce-white');
+    })
+
+    $('.btn-crust').click(()=> {
+        $('.btn-crust').toggleClass('active')
+        $('.crust').toggleClass('crust-gluten-free')
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
