7a92cb92e3d54f08e7ca3816ecf5eda3ddddfcf3
mannysin <sinclair2610@gmail.com>
2018-11-02 17:55:20 -0400

    almost done

--- starter-code/pizza.js
+++ starter-code/pizza.js
@@ -1,8 +1,33 @@
 // Write your Pizza Builder JavaScript in this file.
 window.onload = function(){
 
+    // notes from class - fine with how we did it but this does it to every buttton before hand
+    // $('.btn').click(function(){
+    //     $(this).toggleClass('active');
+    // })
+    $('.btn-sauce, .btn-crust').removeClass('active');
+    $('.sauce').removeClass('sauce-white');
+    $('.crust').removeClass('crust-gluten-free');
+    $('.price li:nth-child(4)').hide()
+    $('.price li:nth-child(5)').hide()
+    
+    let theActualPrice = 13;
+    updatePrice();
+
+    function addOrSubtractFromPrice(cost, buttonPushed){
+        if(buttonPushed.hasClass('active'))
+    }
+
+    function updatePrice(){
+        $('strong').text('$' + theActualPrice)
+    }
+
+    
 
 
+
+
+    ----
     $('.pep').toggle()
     $('.mushroom').toggle()
     $('.green-pepper').toggle()
@@ -12,6 +37,14 @@ window.onload = function(){
     $('.btn-pepperonni').click(()=>{
             $('.btn-pepperonni').toggleClass('active');
             $('.pep').toggle()
+            if($(this).hasClass('active')) {
+                theActualPrice += 1;
+                console.log(theActualPrice)
+            } else {
+                theActualPrie -= 1;
+                console.log(theActualPrice)
+            }
+
             if ($('.btn-pepperonni').hasClass('active')){
                 $('.btn-pepperonni':visible)
             }; else {
@@ -22,12 +55,12 @@ window.onload = function(){
 
     $('.btn-mushrooms').click(()=>{
         $('.btn-mushrooms').toggleClass('active');
-        $('.mushroom').toggle()
+        $('.mushroom').toggle();
     })
 
     $('.btn-green-peppers').click(()=>{
         $('.btn-green-peppers').toggleClass('active');
-        $('.green-pepper').toggle()
+        $('.green-pepper').toggle();
     })
     
     $('.btn-sauce').click(()=> {
@@ -36,8 +69,8 @@ window.onload = function(){
     })
 
     $('.btn-crust').click(()=> {
-        $('.btn-crust').toggleClass('active')
-        $('.crust').toggleClass('crust-gluten-free')
+        $('.btn-crust').toggleClass('active');
+        $('.crust').toggleClass('crust-gluten-free');
     })
 
 
